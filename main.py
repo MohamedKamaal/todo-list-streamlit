423fd0ea5a64440a910e8ecadfdf70308adc434c
MohamedKamaal <mohamedkamal.analytics@gmail.com>
2024-09-12 08:26:30 +0300

    latest look

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st 
 from functions import get_tasks, write_tasks
 st.title('To-do list')
-st.subheader('manage your tasks easily')
 st.write('improve your productivity')
 def add_task():
     new_todo = st.session_state['new_task'] +"\n"
@@ -21,4 +20,4 @@ for index,task in enumerate(tasks):
         del st.session_state[task]
         st.rerun()
     
-st.text_input(label='aa',placeholder='add new task',on_change =add_task,key='new_task')
+st.text_input(label='',placeholder='add new task',on_change =add_task,key='new_task')
